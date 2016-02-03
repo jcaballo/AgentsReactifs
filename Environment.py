b17822dcfa2adc86f6aaee8c491b9b4095c979b7
Adrien74 <adrien.detraz@gmail.com>
2016-02-03 15:15:36 +0100

    main + affichage coordonnées empty object

--- Environment.py
+++ Environment.py
@@ -1,17 +1,12 @@
 from Empty import Empty
-from tkinter import *
 
 class Environment: # Définition de notre classe Personne
 
+    matrice = []
     def __init__(self, _i, _j):
         for i in range(0, _i):
+            self.matrice.append([])
             for j in range(0, _j):
-                self.matrice[i][j] = Empty(i,j);
+                self.matrice[i].append(Empty(i,j))
 
-
-
-    fenetre = Tk()
-    champ_label = Label(fenetre, text="Salut les Zér0s !")
-    champ_label.pack()
-    fenetre.mainloop()
 
