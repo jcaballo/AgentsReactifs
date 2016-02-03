0e04a7ff3a20ab96427c31cb47f05c0c9de52ceb
caballoj <julien.caballol@hotmail.fr>
2016-02-03 14:57:19 +0100

    Fin des problèmes de modules

--- Rock.py
+++ Rock.py
@@ -1,5 +1,5 @@
-import Content
+from Content import Content
 
 class Rock(Content):
     def __init__(self, x, y):
-        super(Rock, self).__init__(x,y);
+        super(Rock, self).__init__(x, y)
