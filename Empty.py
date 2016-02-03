6ae133bb722b66a2dc17a19fb97e328cf22cb38d
Adrien74 <adrien.detraz@gmail.com>
2016-02-03 14:54:40 +0100

    problèmes de modules

--- Empty.py
+++ Empty.py
@@ -1,5 +1,5 @@
-import Content
+from Content import Content
 
 class Empty(Content):
     def __init__(self, x, y):
-        super(Empty, self).__init__(x,y);
+        super().__init__(x,y);
--- Environment.py
+++ Environment.py
@@ -1,9 +1,17 @@
-import Empty
+from Empty import Empty
+from tkinter import *
 
 class Environment: # Définition de notre classe Personne
 
     def __init__(self, _i, _j):
         for i in range(0, _i):
             for j in range(0, _j):
-                self.matrice[i][j] = Empty();
+                self.matrice[i][j] = Empty(i,j);
+
+
+
+    fenetre = Tk()
+    champ_label = Label(fenetre, text="Salut les Zér0s !")
+    champ_label.pack()
+    fenetre.mainloop()
 
